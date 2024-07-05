db1bb4456dac31146d34543b19008e53476bb3d9
Adam Sohn <adamsohn@MacBook-Pro-3.local>
2024-07-05 10:41:13 +0800

    Added browser extension app to monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,33 @@
 module.exports = {
-  extends: "@nighttrax/eslint-config-tsx",
+  extends: ["@nighttrax/eslint-config-tsx", "plugin:prettier/recommended"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        doubleQuote: true,
+      },
+    ],
+    "react/react-in-jsx-scope": "off", // Disable the rule that requires React to be in scope,
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/vite.config.ts", // Include TypeScript config file
+        ],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the react version
+    },
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.ts", "*.jsx", "*.tsx"],
+      rules: {
+        quotes: ["error", "double"],
+      },
+    },
+  ],
 };
